83aa3106fdd77e10884304ff59173c6bf4ccd334
Chansanith <33217719+Chansanith@users.noreply.github.com>
2023-07-17 03:22:21 +0700

    Update khmernamegenerator.js

--- khmernamegenerator.js
+++ khmernamegenerator.js
@@ -1,22 +1,36 @@
-var NumberOfWords = 13
+var NumberOfWords = 27
 
 var words = new BuildArray(NumberOfWords)
 
-words[1] = "Guilty Crown <br> Sak <br> Sok"
-words[2] = "Overlord <br> Sak <br> Sok"
-words[3] = "Danganronpa <br> Sak <br> Sok"
-words[4] = "Needless <br> Sak <br> Sok"
-words[5] = "Heavy Object <br> Sak <br> Sok"
-words[6] = "Attack on Titan"
-words[7] = "Dragon Ball Super"
-words[8] = "Jojo's Bizarre Adventure"
-words[9] = "Persona 5"
-words[10] = "Chan Nith"
-words[11] = "Riki Tag"
-words[12] = "Manith Ne"
-words[13] = "Sok"
+words[1] = "Vet Thida"
+words[2] = "In Sarum"
+words[3] = "Sok Chetra"
+words[4] = "Mouern Lida"
+words[5] = "Seng Sreyneang"
+words[6] = "Ly Sreyven"
+words[7] = "Seth Soklin"
+words[8] = "Chhor Sokhourng"
+words[9] = "Chek Chhay"
+words[10] = "Kim Chandy"
+words[11] = "Mak Ratanakbot"
+words[12] = "Koem Tangkea"
+words[13] = "Pheng Lakhena"
+words[14] = "Khin Thavy"
+words[15] = "Chan Sreymom"
+words[16] = "Phiv Sovann"
+words[17] = "Chhoem Malen"
+words[18] = "Lay Sreypich"
+words[19] = "Sreang Reaksaa"
+words[20] = "Sat Leaphea"
+words[21] = "Nov Thida"
+words[22] = "Nan Sreythea"
+words[23] = "Chheang Simuoy"
+words[24] = "Yoeurn Sophaphit"
+words[25] = "Lim Panha"
+words[26] = "Leang Sonyta"
+words[27] = "Toek Kanhchana"
 
-//thanks @Pete Hearne for the function!//
+//Function!//
 function BuildArray(size){
   this.length = size
   for (var i = 1; i <= size; i++){
